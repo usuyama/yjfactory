6495c7627c306688d87a192fa1b0793430c08483
Usuyama Naoto <usuyama@UoT-IST-Ub.(none)>
2010-10-24 02:16:34 +0900

    すこーし変更。配列のアクセスをオフセットを直接指定するようにした。明日がんばる

--- compiler/min-caml-0.1.0/virtual.ml
+++ compiler/min-caml-0.1.0/virtual.ml
@@ -92,8 +92,8 @@ let rec g env = function (* 式の仮想マシンコード生成 (caml2html: virtual_g) *)
 	expand
 	  (List.map (fun x -> (x, M.find x env)) xs)
 	  (0, Ans(Mov(y)))
-	  (fun x offset store -> seq(StDF(x, y, C(offset)), store))
-	  (fun x _ offset store -> seq(St(x, y, C(offset)), store)) in
+	  (fun x offset store -> seq(StF(x, y, offset), store))
+	  (fun x _ offset store -> seq(St(x, y, offset), store)) in
       Let((y, Type.Tuple(List.map (fun x -> M.find x env) xs)), Mov(reg_hp),
 	  Let((reg_hp, Type.Int), Add(reg_hp, C(align offset)),
 	      store))
@@ -105,44 +105,38 @@ let rec g env = function (* 式の仮想マシンコード生成 (caml2html: virtual_g) *)
 	  (0, g (M.add_list xts env) e2)
 	  (fun x offset load ->
 	    if not (S.mem x s) then load else (* [XX] a little ad hoc optimization *)
-	    fletd(x, LdDF(y, C(offset)), load))
+	    fletd(x, LdF(y, offset), load))
 	  (fun x t offset load ->
 	    if not (S.mem x s) then load else (* [XX] a little ad hoc optimization *)
-	    Let((x, t), Ld(y, C(offset)), load)) in
+	    Let((x, t), Ld(y, offset), load)) in
       load
   | Closure.Get(x, y) -> (* 配列の読み出し (caml2html: virtual_get) *)
-      let offset = Id.genid "o" in
       (match M.find x env with
       | Type.Array(Type.Unit) -> Ans(Nop)
       | Type.Array(Type.Float) ->
-	  Let((offset, Type.Int), SLL(y, C(3)),
-	      Ans(LdDF(x, V(offset))))
+	  Ans(LdF(x, y))
       | Type.Array(_) ->
-	  Let((offset, Type.Int), SLL(y, C(2)),
-	      Ans(Ld(x, V(offset))))
+	  Ans(Ld(x, y))
       | _ -> assert false)
   | Closure.Put(x, y, z) ->
-      let offset = Id.genid "o" in
       (match M.find x env with
       | Type.Array(Type.Unit) -> Ans(Nop)
       | Type.Array(Type.Float) ->
-	  Let((offset, Type.Int), SLL(y, C(3)),
-	      Ans(StDF(z, x, V(offset))))
+	      Ans(StDF(z, x, y))
       | Type.Array(_) ->
-	  Let((offset, Type.Int), SLL(y, C(2)),
-	      Ans(St(z, x, V(offset))))
+	      Ans(St(z, x, y))
       | _ -> assert false)
-  | Closure.ExtArray(Id.L(x)) -> Ans(SetL(Id.L("min_caml_" ^ x)))
+  | Closure.ExtArray(Id.L(x)) -> Ans(SetL(Id.L("yj_" ^ x)))
 
 (* 関数の仮想マシンコード生成 (caml2html: virtual_h) *)
 let h { Closure.name = (Id.L(x), t); Closure.args = yts; Closure.formal_fv = zts; Closure.body = e } =
-  let (int, float) = separate yts in
+  let (int, float) = separate yts in (* closureのアドレスをセットした後、引数を展開 *)
   let (offset, load) =
     expand
       zts
-      (4, g (M.add x t (M.add_list yts (M.add_list zts M.empty))) e)
-      (fun z offset load -> fletd(z, LdDF(reg_cl, C(offset)), load))
-      (fun z t offset load -> Let((z, t), Ld(reg_cl, C(offset)), load)) in
+      (1, g (M.add x t (M.add_list yts (M.add_list zts M.empty))) e)
+      (fun z offset load -> fletd(z, LdF(reg_cl, offset), load))
+      (fun z t offset load -> Let((z, t), Ld(reg_cl, offset), load)) in
   match t with
   | Type.Fun(_, t2) ->
       { name = Id.L(x); args = int; fargs = float; body = load; ret = t2 }
