d70d588e8cb13637208bc9a514bae16761c3a250
Usuyama Naoto <usuyama@UoT-IST-Ub.(none)>
2010-10-25 18:35:40 +0900

    ライブラリ関数呼び出しを訂正

--- compiler/min-caml-0.1.0/emit.ml
+++ compiler/min-caml-0.1.0/emit.ml
@@ -135,7 +135,7 @@ and g' oc = function (* 各命令のアセンブリ生成 (caml2html: emit_gprime) *)
       fprintf oc "\tjr\t%s\n" reg_sw;
   | Tail, CallDir(Id.L(x), ys, zs) -> (* 末尾呼び出し *)
       g'_args oc [] ys zs;
-      fprintf oc "\tj\t%s\n" x;
+      fprintf oc "\tjal\t%s\n" x;
   | NonTail(a), CallCls(x, ys, zs) ->
       g'_args oc [(x, reg_cl)] ys zs;
       let ss = stacksize () in
