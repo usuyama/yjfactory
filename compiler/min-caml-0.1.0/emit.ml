61e2d8c62a5ef2d04da652a750bef98823e4ebf4
Usuyama Naoto <usuyama@UoT-IST-Ub.(none)>
2010-10-30 04:20:20 +0900

    add hp initialize

--- compiler/min-caml-0.1.0/emit.ml
+++ compiler/min-caml-0.1.0/emit.ml
@@ -204,6 +204,7 @@ let f oc (Prog(fundefs, e)) =
   fprintf oc "entry:\n";
   print_li oc reg_sp 0;
   print_li oc reg_ra 0;
+  print_li oc reg_ra 10000;
   stackset := S.empty;
   stackmap := [];
   g oc (Tail, e);
