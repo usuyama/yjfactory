050ee5baa8b0a56e03e0eb97f7acf17b41f64a4c
kohei shimane <shimane.kh@gmail.com>
2010-11-01 17:58:22 +0900

    assembler bgtf

--- simulater/Assembler.java
+++ simulater/Assembler.java
@@ -265,6 +265,11 @@ public class Assembler {
 			    code.append(gr.getRegnum(inst[2]));
 			    code.append(gr.signInt_toStr_nlen(tagmap.get(inst[3])-k, 16));
 			}
+			else if(inst[0].equals("bgtf")){
+			    code.append("XXBGTF");
+			    code.append(gr.getFnum(inst[1]));
+			    code.append(gr.getFnum(inst[2]));
+			    code.append(gr.signInt_toStr_nlen(tagmap.get(inst[3])-k, 16));
 			else if(inst[0].equals("jal")){
 			    code.append("010110");
 			    code.append(gr.toStr_nlen(tagmap.get(inst[1]), 26));
