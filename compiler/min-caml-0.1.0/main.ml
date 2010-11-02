67d9f25e92269b82ff5cb5f1709f73895ba2cf71
Usuyama Naoto <usuyama@UoT-IST-Ub.(none)>
2010-11-02 15:42:10 +0900

    noprintオプションをつけた

--- compiler/min-caml-0.1.0/main.ml
+++ compiler/min-caml-0.1.0/main.ml
@@ -1,5 +1,6 @@
 (*-*-coding:euc-jp-*-*)
 let limit = ref 1000
+let noprint_r = ref false
 
 let rec iter n e = (* 最適化処理をくりかえす (caml2html: main_iter) *)
   Format.eprintf "iteration %d@." n;
@@ -9,21 +10,35 @@ let rec iter n e = (* 最適化処理をくりかえす (caml2html: main_iter) *)
 	iter (n - 1) e'
 
 let lexbuf outchan l = (* バッファをコンパイルしてチャンネルへ出力する (caml2html: main_lexbuf) *)
-  Id.counter := 0;
-  Typing.extenv := M.empty;
-  Emit.f outchan
-    (Asm.print "RegAlloc"
+  if !noprint_r then
+    (Id.counter := 0;
+     Typing.extenv := M.empty;
+     Emit.f outchan
        (RegAlloc.f
-	  (Asm.print "Virtual"
-		   (Virtual.f
-		      (Closure.print 
+	  (Virtual.f
+	     (Closure.f
+		(iter !limit
+		   (Alpha.f
+		      (KNormal.f
+			 (Typing.f
+			    (Parser.exp Lexer.token l)))))))))
+ else
+   (Id.counter := 0;
+    Typing.extenv := M.empty;
+    Emit.f outchan
+      (Asm.print "RegAlloc"
+	 (RegAlloc.f
+	    (Asm.print "Virtual"
+	       (Virtual.f
+		  (Closure.print 
 			 (Closure.f
 			    (iter !limit
 			       (Alpha.f
 				  (KNormal.print
 				     (KNormal.f
 					(Typing.f
-					      (Parser.exp Lexer.token l))))))))))))
+					   (Syntax.print
+					      (Parser.exp Lexer.token l))))))))))))))
     
 let string s = lexbuf stdout (Lexing.from_string s) (* 文字列をコンパイルして標準出力に表示する (caml2html: main_string) *)
 
@@ -36,14 +51,16 @@ let file f = (* ファイルをコンパイルしてファイルに出力する (caml2html: main_file
       close_out outchan;
     with e -> (close_in inchan; close_out outchan; raise e)
 
+
 let () = (* ここからコンパイラの実行が開始される (caml2html: main_entry) *)
   let files = ref [] in
     Arg.parse
       [("-inline", Arg.Int(fun i -> Inline.threshold := i), "maximum size of functions inlined");
-       ("-iter", Arg.Int(fun i -> limit := i), "maximum number of optimizations iterated")]
-      (fun s -> files := !files @ [s])
+       ("-iter", Arg.Int(fun i -> limit := i), "maximum number of optimizations iterated");
+       ("--noprint", Arg.Set noprint_r ,"")]
+       (fun s -> files := !files @ [s])
       ("Mitou Min-Caml Compiler (C) Eijiro Sumii\n" ^
-	 Printf.sprintf "usage: %s [-inline m] [-iter n] ...filenames without \".ml\"..." Sys.argv.(0));
+	  Printf.sprintf "usage: %s [-inline m] [-iter n] ...filenames without \".ml\"..." Sys.argv.(0));
     List.iter
       (fun f -> ignore (file f))
       !files
