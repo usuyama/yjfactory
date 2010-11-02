e6ffd0be4f228056ddfbf189b339414a31fc5ba6
s101026 <s101026@UoT-IST-Ub.(none)>
2010-11-02 12:27:29 +0900

    fsqrt(ちょっとおかしい)

--- hardware/hemmi/FSQRT.vhd
+++ hardware/hemmi/FSQRT.vhd
@@ -4,10 +4,9 @@ use IEEE.std_logic_arith.all;
 use IEEE.std_logic_unsigned.all;
 
 entity FSQRT is
-  port(clk : in std_logic;
+  port(clk,enable : in std_logic;
        A : in std_logic_vector(31 downto 0);
-       R : out std_logic_vector(31 downto 0);
-		 AO : out std_logic_vector(31 downto 0));
+       R : out std_logic_vector(31 downto 0));
 end entity FSQRT;
 
 architecture STRUCTURE of FSQRT is
@@ -19,44 +18,31 @@ component FMUL is
   port (A,B : in std_logic_vector(31 downto 0);
         P : out std_logic_vector(31 downto 0));
 end component FMUL;
-signal NA,irt,irt2,irt3,irt4,irt5,rt,AP : std_logic_vector(31 downto 0);
+signal rt,irt,irt2,irt3,irt4,irt5,NA,AP : std_logic_vector(31 downto 0);
 begin
-p0 : process(A) is
-variable E : integer;
-begin
-  NA(31) <= not A(31);
-  NA(30 downto 23) <= A(30 downto 23) - "00000001";
-  NA(22 downto 0) <= A(22 downto 0);
-  E := conv_integer(A(30 downto 23));
-  irt(31) <= A(31);
-  if (E = 0) then
-    irt(30 downto 0) <= "1111111100000000000000000000000";
-  elsif (E = 127) then
-    irt(30 downto 0) <= A(30 downto 0);
-  else
-    if (A(23) = '1') then
-	   E := E - 1;
-		irt(22) <= '0';
-    else
-		irt(22) <= '1';
-	 end if;
-	 E := 190 - E/2;
-    irt(30 downto 23) <= conv_std_logic_vector(E,8);
-    irt(21 downto 0) <= "0000000000000000000000";
-  end if;
-end process p0;
-p1 : process(clk) is
+p0 : process(clk,A) is
 begin
   if (rising_edge(clk)) then
-      R <= rt;
+    if (enable = '1') then
+      NA(31) <= not A(31);
+      NA(30 downto 23) <= A(30 downto 23) - "00000001";
+      NA(22 downto 0) <= A(22 downto 0);
+      irt(31) <= A(31);
+      irt(30 downto 23) <= "10111110" - ('0' & A(30 downto 24));
+      if (A(23) = '1') then
+        irt(22) <= '0';
+      else
+        irt(22) <= '1';
+      end if;
+      irt(21 downto 0) <= "0000000000000000000000";
+    else
       irt <= irt5;
+    end if;
   end if;
-end process p1;
-AO <= irt;
+end process p0;
 MUL0 : FMUL port map (irt,irt,irt2);
 MUL1 : FMUL port map (NA,irt2,irt3);
 ADD0 : FADD port map (irt3,"00111111110000000000000000000000",irt4);
 MUL2 : FMUL port map (irt4,irt,irt5);
-MUL3 : FMUL port map (irt5,A,rt);
-MUL4 : FMUL port map (rt,rt,AP);
+MUL3 : FMUL port map (irt5,A,R);
 end STRUCTURE;
