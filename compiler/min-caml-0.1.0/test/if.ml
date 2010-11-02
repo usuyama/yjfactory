92105e5643eafeb8bab2a4e5aba1fb584bd640e3
Usuyama Naoto <usuyama@UoT-IST-Ub.(none)>
2010-11-02 15:43:54 +0900

    test folder整理

--- compiler/min-caml-0.1.0/test/if.ml
+++ compiler/min-caml-0.1.0/test/if.ml
@@ -18,7 +18,7 @@ else
   print_int 1
 else
   print_int 0);
-(if 1 != 1 then
+(if 1 <> 1 then
   print_int 0
 else
   print_int 1);
--- compiler/min-caml-0.1.0/test/rarara.s
+++
@@ -1,9 +0,0 @@
-	j	entry
-entry:
-	li	%i2, 4
-	st	ra, [sp + 4]
-	call	min_caml_print_int
-	add	sp, 8, sp	# delay slot
-	sub	sp, 8, sp
-	ld	[sp + 4], ra
-	halt
