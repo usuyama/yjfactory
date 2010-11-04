a832b51187a0e7bec85d825c15cee287e507b0b1
Usuyama Naoto <usuyama@UoT-IST-Ub.(none)>
2010-11-04 16:59:53 +0900

    libmin.s

---
+++ lib/libmin.s
@@ -0,0 +1,53 @@
+yj_print_int:
+	sendw	%r1
+	jr	%ra
+yj_print_float:
+	movf2i	%r1, %f0
+	sendw	%r1
+	jr	%ra
+yj_print_char:
+	sendc	%r1
+	jr	%ra
+yj_create_array:
+	lli	%r3, 0
+yj_create.loop:
+	add	%r4, %hp, %r3
+	sw	%r2, [%r4 + 0]
+	addi	%r3, %r3, 1
+	bgt	%r1, %r3, yj_create.loop
+	add	%r1, %r0, %hp
+	add	%hp, %hp, %r3
+	jr	%ra
+yj_sin:
+	sin	%f0, %f0
+	jr	%ra
+yj_cos:
+	cos	%f0, %f0
+	jr	%ra
+yj_atan:
+	atan	%f0, %f0
+	jr	%ra
+yj_float_of_int:
+	itof	%f0, %r1
+	jr	%ra
+yj_int_of_float:
+	ftoi	%r1, %f0
+	jr	%ra
+yj_sqrt:
+	sqrt	%f0, %f0
+	jr	%ra
+yj_floor:
+	sqrt	%f0, %f0
+	jr	%ra
+yj_read_int:
+	read_int	%r1
+	jr	%ra
+yj_read_float:
+	read_float	%f0
+	jr	%ra
+yj_fabs:
+	absf	%f0, %f0
+	jr	%ra
+yj_fneg:
+	negf	%f0, %f0
+	jr	%ra%
