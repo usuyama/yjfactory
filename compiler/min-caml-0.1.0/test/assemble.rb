793b66418928b0781d10adf88c8422fcb7655f2f
Usuyama Naoto <usuyama@UoT-IST-Ub.(none)>
2010-11-07 03:09:19 +0900

    assemble.rb

--- compiler/min-caml-0.1.0/test/assemble.rb
+++ compiler/min-caml-0.1.0/test/assemble.rb
@@ -4,7 +4,7 @@ fullname = ARGV[0]
 program_name = File::basename(fullname, '.ml')
 sh = Shell.cd "."
 without_ext = fullname[0..fullname.length-4]
-unless system "../min-caml --noprint " + without_ext
+unless ARGV[1] || system "../min-caml --noprint " + without_ext
   print "XXX compile failed\n"
 else
   system "cat " + "../../../lib/libmin.s >> " + without_ext + ".s"
@@ -18,7 +18,7 @@ else
   else
     print "###simulater###\n"
     system "rm -f outlog"
-    unless system "../../../simulater/guitocui/simulator o ../../../simulater/" + program_name + " sld/base.sld"
+    unless system "../../../simulater/guitocui/simulator o ../../../simulater/" + program_name + " sld/contest.sld"
       print "XXX simulate failed\n"
     else
       print "###output###\n"
