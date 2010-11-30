dc4d0c5feef5c1bd776e2c217711e07ba0ff189c
Usuyama Naoto <usuyama@UoT-IST-Ub.(none)>
2010-11-30 16:51:30 +0900

    rename and fix exec.rb

--- compiler/min-caml-0.1.0/test/exec.rb
+++ compiler/min-caml-0.1.0/test/exec.rb
@@ -1,10 +1,14 @@
 require "shell"
 
+unless ARGV[0]
+  print "usage: ruby assemble.rb program\n"
+  exit
+end
 fullname = ARGV[0]
 program_name = File::basename(fullname, '.ml')
 sh = Shell.cd "."
 without_ext = fullname[0..fullname.length-4]
-unless ARGV[1] || system "../min-caml --noprint " + without_ext
+unless ARGV[1] || (system "../min-caml --noprint " + without_ext)
   print "XXX compile failed\n"
 else
   system "cat " + "../../../lib/libmin.s >> " + without_ext + ".s"
@@ -16,6 +20,7 @@ else
   unless sh.transact{system "java", "Assembler", program_name + ".s", program_name}
     print "XXX assemle failed\n"
   else
+    system "cp ../../../simulater/" + program_name + " " + without_ext
     print "###simulater###\n"
     system "rm -f outlog"
     unless system "../../../simulater/guitocui/simulator o ../../../simulater/" + program_name + " sld/contest.sld"
