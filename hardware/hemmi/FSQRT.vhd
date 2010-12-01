204098b64c7b063aa55b02be86f9b616bb663293
hemmi <s101026@UoT-IST-Ub.(none)>
2010-12-01 12:04:28 +0900

    fsqrt,fsub

--- hardware/hemmi/FSQRT.vhd
+++ hardware/hemmi/FSQRT.vhd
@@ -18,36 +18,49 @@ component FMUL is
   port (A,B : in std_logic_vector(31 downto 0);
         P : out std_logic_vector(31 downto 0));
 end component FMUL;
-signal irt,irt0,irt1,irt2,irt22,irt31,irt32,irt4,irt42,irt5,irt52,irt6,NA : std_logic_vector(31 downto 0);
+signal NX,X1,X2,X3,X4,Z1,Z2,Z3,Z4,D,D2,D3,DD,DD2 : std_logic_vector(31 downto 0);
 begin
 p0 : process(MCLK1) is
 begin
   if (rising_edge(MCLK1)) then
     if (ready = '1') then
-      NA(31) <= '1';
-      NA(22 downto 0) <= A(22 downto 0);
-		irt(31) <= '0';
-      irt(22 downto 0) <= "00000000000000000000000";
+      NX(31) <= '1';
+      NX(22 downto 0) <= A(22 downto 0);
+		X1(31) <= '0';
+		Z1(31) <= '0';
+      X1(22 downto 0) <= A(22 downto 0);
+      Z1(22 downto 0) <= A(22 downto 0);
 		if (A(23) = '1') then
-		  irt(30 downto 23) <= "01111111";
-		  NA(30 downto 23)  <= "01111110";
+		  X1(30 downto 23) <= "01111111";
+		  Z1(30 downto 23) <= "01111111";
+		  NX(30 downto 23) <= "01111110";
 		else
-		  irt(30 downto 23) <= "01111110";
-		  NA(30 downto 23)  <= "01111101";
+		  X1(30 downto 23) <= "10000000";
+		  Z1(30 downto 23) <= "10000000";
+		  NX(30 downto 23) <= "01111111";
 		end if;
 	 else
-	   irt <= irt5;
+	   NX <= (not X4(31)) & (X4(30 downto 23) - "00000001") & X4(22 downto 0);
+		X1 <= X4;
+		Z1 <= Z4;
+		X2 <= X1;
+		Z2 <= Z1;
+	   D2 <= D;
+		X3 <= X2;
+		Z3 <= Z2;
+		D3 <= D2;
+		DD2 <= DD;
     end if;
   end if;
 end process p0; 
-irt6(31) <= A(31);
-irt6(30 downto 23) <= "00000000" when A(30 downto 23) = "00000000" else
-                      "11111111" when A(30 downto 23) = "11111111" else
-						    irt5(30 downto 23) - ('0' & A(30 downto 24)) + "00111111";
-irt6(22 downto 0) <= irt5(22 downto 0);
-MUL0 : FMUL port map (irt,irt,irt2);
-MUL1 : FMUL port map (NA,irt2,irt31);
-ADD0 : FADD port map (irt31,"00111111110000000000000000000000",irt4);
-MUL2 : FMUL port map (irt4,irt,irt5);
-MUL3 : FMUL port map (irt6,A,R);
+R(31) <= A(31);
+R(30 downto 23) <= "00000000" when A(30 downto 23) = "00000000" else
+                   "11111111" when A(30 downto 23) = "11111111" else
+                   "01000000" + ('0' & A(30 downto 24)) when A(23) = '1' else
+                   "00111111" + ('0' & A(30 downto 24));
+R(22 downto 0) <= Z4(22 downto 0);
+ADD0 : FADD port map (NX,"00111111110000000000000000000000",D);
+MUL0 : FMUL port map (D2,D2,DD);
+MUL1 : FMUL port map (X3,DD2,X4);
+MUL2 : FMUL port map (Z3,D3,Z4);
 end STRUCTURE;
---
+++ hardware/hemmi/FSUB.vhd
@@ -0,0 +1,84 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.std_logic_arith.all;
+use IEEE.std_logic_unsigned.all;
+
+entity FSUB is
+  port (A,B : in std_logic_vector(31 downto 0);
+        S : out std_logic_vector(31 downto 0));
+end FSUB;
+
+architecture GATE of FSUB is
+begin
+p0 : process (A,B) is
+variable NotB,G,L : std_logic_vector(31 downto 0);
+variable GF,LF : std_logic_vector(48 downto 0);
+variable R : std_logic_vector(22 downto 0);
+variable F : std_logic_vector(73 downto 0);
+variable Sign : std_logic;
+variable E,P,SE : integer;
+begin
+  NotB := (not B(31)) & B(30 downto 0);
+  if (conv_integer(B(30 downto 0)) > conv_integer(A(30 downto 0))) then
+    G := NotB;
+    L := A;
+  else
+    G := A;
+    L := NotB;
+  end if;
+  E := conv_integer(G(30 downto 23) - L(30 downto 23));
+
+  if (E > 24) then
+    S <= G;
+  else
+	 GF := "01" & G(22 downto 0) & "000000000000000000000000";
+	 LF := conv_std_logic_vector(0,49);
+	 LF(47-E downto 24-E) := '1' & L(22 downto 0);
+
+    F(73) := '0';
+	 F(23 downto 0) := "000000000000000000000000";
+	 Sign := G(31) xor L(31);
+    if (Sign = '0') then
+	   F(72 downto 24) := GF + LF;
+    else
+	   F(72 downto 24) := GF - LF;
+    end if;
+
+    P := 73;
+	 SE := 73;
+    for I in 24 to 72 loop
+	   if (F(I) = '1') then
+		  if (SE = 73) then
+		    SE := I;
+		  end if;
+		  P := I;
+		end if;
+    end loop;
+
+    F(72 downto 48) := F(P downto P-24);
+    if (F(48) = '1' and ((P - SE > 24) or (F(49) = '1'))) then
+	   F(73 downto 49) := F(73 downto 49) + "0000000000000000000000001";
+    end if;
+	 if (P = 73) then
+	   S <= "00000000000000000000000000000000";
+	 else
+	   S(31) <= G(31);
+      if (F(73) = '1') then
+		  E := conv_integer(G(30 downto 23)) + P - 70;
+		  R(22 downto 0) := F(72 downto 50);
+		else
+		  E := conv_integer(G(30 downto 23)) + P - 71;
+		  R(22 downto 0) := F(71 downto 49);
+		end if;
+		if (E >= 255) then
+		  S(30 downto 0) <= "1111111100000000000000000000000";
+		elsif (E <= 0) then
+		  S(30 downto 0) <= "0000000000000000000000000000000";
+		else
+		  S(30 downto 23) <= conv_std_logic_vector(E,8);
+        S(22 downto 0) <= R(22 downto 0);
+		end if;
+    end if;
+  end if;
+end process p0;
+end GATE;
