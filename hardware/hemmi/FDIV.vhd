318fd3404cd3d02129329a6e0813f8bbcaf6791f
hemmi <s101026@UoT-IST-Ub.(none)>
2010-12-02 12:40:54 +0900

    FMUL高速化

--- hardware/hemmi/FDIV.vhd
+++ hardware/hemmi/FDIV.vhd
@@ -22,28 +22,28 @@ p0 : process(MCLK1) is
 begin
   if (rising_edge(MCLK1)) then
     if (ready = '1') then
-	   Sign <= A(31) xor B(31);
-	   X1(31) <= '0';
-		if (A(30 downto 23) = "00000000" or B(30 downto 23) = "11111111") then
-		  E <= 0;
-		elsif (A(30 downto 23) = "11111111" or B(30 downto 23) = "00000000") then
-		  E <= 255;
-	   elsif (A(22 downto 0) <= B(22 downto 0)) then
-		  X1(30 downto 23) <= "10000000";
-		  E <= conv_integer(A(30 downto 23)) - conv_integer(B(30 downto 23)) + 126;
-		else
-		  X1(30 downto 23) <= "01111111";
-		  E <= conv_integer(A(30 downto 23)) - conv_integer(B(30 downto 23)) + 127;
-		end if;
+      Sign <= A(31) xor B(31);
+      X1(31) <= '0';
+      if (A(30 downto 23) = "00000000" or B(30 downto 23) = "11111111") then
+        E <= 0;
+      elsif (A(30 downto 23) = "11111111" or B(30 downto 23) = "00000000") then
+        E <= 255;
+      elsif (A(22 downto 0) <= B(22 downto 0)) then
+        X1(30 downto 23) <= "10000000";
+        E <= conv_integer(A(30 downto 23)) - conv_integer(B(30 downto 23)) + 126;
+      else
+        X1(30 downto 23) <= "01111111";
+        E <= conv_integer(A(30 downto 23)) - conv_integer(B(30 downto 23)) + 127;
+      end if;
       X1(22 downto 0) <= A(22 downto 0);
-		Y1 <= "101111111" & B(22 downto 0);
-	 else
-	   X1 <= X4;
-		Y1 <= Y4;
-		X3 <= X1;
-		Y3 <= Y1;
-		D3 <= D2;
-	 end if;
+      Y1 <= "101111111" & B(22 downto 0);
+    else
+      X1 <= X4;
+      Y1 <= Y4;
+      X3 <= X1;
+      Y3 <= Y1;
+      D3 <= D2;
+    end if;
   end if;
 end process p0;
 R <= Sign & conv_std_logic_vector(E,8) & X4(22 downto 0);
--- hardware/hemmi/FSQRT.vhd
+++ hardware/hemmi/FSQRT.vhd
@@ -24,41 +24,41 @@ p0 : process(MCLK1) is
 begin
   if (rising_edge(MCLK1)) then
     if (ready = '1') then
-	   AR <= A;
+      AR <= A;
       NX(31) <= '1';
       NX(22 downto 0) <= A(22 downto 0);
-		X1(31) <= '0';
-		Z1(31) <= '0';
+      X1(31) <= '0';
+      Z1(31) <= '0';
       X1(22 downto 0) <= A(22 downto 0);
       Z1(22 downto 0) <= A(22 downto 0);
-		if (A(23) = '1') then
-		  X1(30 downto 23) <= "01111111";
-		  Z1(30 downto 23) <= "01111111";
-		  NX(30 downto 23) <= "01111110";
-		else
-		  X1(30 downto 23) <= "10000000";
-		  Z1(30 downto 23) <= "10000000";
-		  NX(30 downto 23) <= "01111111";
-		end if;
-	 else
-	   NX <= (not X4(31)) & (X4(30 downto 23) - "00000001") & X4(22 downto 0);
-		X1 <= X4;
-		Z1 <= Z4;
-		X2 <= X1;
-		Z2 <= Z1;
-	   D2 <= D;
-		X3 <= X2;
-		Z3 <= Z2;
-		D3 <= D2;
-		DD2 <= DD;
+      if (A(23) = '1') then
+        X1(30 downto 23) <= "01111111";
+        Z1(30 downto 23) <= "01111111";
+        NX(30 downto 23) <= "01111110";
+      else
+        X1(30 downto 23) <= "10000000";
+        Z1(30 downto 23) <= "10000000";
+        NX(30 downto 23) <= "01111111";
+      end if;
+   else
+     NX <= (not X4(31)) & (X4(30 downto 23) - "00000001") & X4(22 downto 0);
+     X1 <= X4;
+     Z1 <= Z4;
+     X2 <= X1;
+     Z2 <= Z1;
+     D2 <= D;
+     X3 <= X2;
+     Z3 <= Z2;
+     D3 <= D2;
+     DD2 <= DD;
     end if;
   end if;
 end process p0; 
 R(31) <= AR(31);
 R(30 downto 23) <= "00000000" when AR(30 downto 23) = "00000000" else
-                   "11111111" when AR(30 downto 23) = "11111111" else
-                   "01000000" + ('0' & A(30 downto 24)) when AR(23) = '1' else
-                   "00111111" + ('0' & A(30 downto 24));
+                    "11111111" when AR(30 downto 23) = "11111111" else
+                    "01000000" + ('0' & A(30 downto 24)) when AR(23) = '1' else
+                    "00111111" + ('0' & A(30 downto 24));
 R(22 downto 0) <= Z4(22 downto 0);
 ADD0 : FADD port map (NX,"00111111110000000000000000000000",D);
 MUL0 : FMUL port map (D2,D2,DD);
