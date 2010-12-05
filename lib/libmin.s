120e99507f1cb3ad6c1bdc27bd13efe8a4db8a83
Usuyama Naoto <usuyama@UoT-IST-Ub.(none)>
2010-12-06 00:24:26 +0900

    fib libmin

--- lib/libmin.s
+++ lib/libmin.s
@@ -49,22 +49,23 @@ yj_read_int:
 	lli	%r1, 0
 	lli	%r2, 0
 	recv	%r1
-	sll	%r1, 8
+	sll	%r1, %r1, 8
 	recv	%r2
 	xor	%r1, %r1, %r2
-	sll	%r1, 8
+	sll	%r1, %r1, 8
 	recv	%r2
 	xor	%r1, %r1, %r2
-	sll	%r1, 8
+	sll	%r1, %r1, 8
 	recv	%r2
 	xor	%r1, %r1, %r2
 	jr	%ra
 yj_read_float:
-	read_float	%f0
+	jal	yj_read_int
+	movi2f	%f0, %r1
 	jr	%ra
 yj_fabs:
 	absf	%f0, %f0
 	jr	%ra
 yj_fneg:
 	negf	%f0, %f0
-	jr	%ra%
+	jr	%ra
