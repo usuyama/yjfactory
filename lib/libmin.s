2387fa01457af5970e7bbb0de2c4cc6ccf9c6064
Usuyama Naoto <usuyama@UoT-IST-Ub.(none)>
2010-12-20 17:44:38 +0900

    lib update

--- lib/libmin.s
+++ lib/libmin.s
@@ -1,6 +1,3 @@
-yj_print_float:
-	print_float	%f0
-	jr	%ra
 yj_print_char:
 	sendc	%r1
 	jr	%ra
@@ -23,15 +20,6 @@ yj_create_float.loop:
 	bgt	%r1, %r3, yj_create_float.loop
 	add	%r1, %r0, %hp
 	add	%hp, %hp, %r3
-	jr	%ra
-yj_sin:
-	sin	%f0, %f0
-	jr	%ra
-yj_cos:
-	cos	%f0, %f0
-	jr	%ra
-yj_atan:
-	atan	%f0, %f0
 	jr	%ra
 yj_float_of_int:
 	itof	%f0, %r1
@@ -60,7 +48,19 @@ yj_read_int:
 	xor	%r1, %r1, %r2
 	jr	%ra
 yj_read_float:
-	read_float	%f0
+	lli	%r1, 0
+	lli	%r2, 0
+	recv	%r1
+	sll	%r1, %r1, 8
+	recv	%r2
+	xor	%r1, %r1, %r2
+	sll	%r1, %r1, 8
+	recv	%r2
+	xor	%r1, %r1, %r2
+	sll	%r1, %r1, 8
+	recv	%r2
+	xor	%r1, %r1, %r2
+	movi2f	%f0, %r1
 	jr	%ra
 yj_fabs:
 	absf	%f0, %f0
